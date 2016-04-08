f292387d6667128b172321bc631c95e441669843
vicky98 <vikiga98@gmail.com>
2016-04-08 15:20:40 +0300

    Some updates...

--- app/src/main/java/project/tp/whattocook/Signup.java
+++ app/src/main/java/project/tp/whattocook/Signup.java
@@ -7,11 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,51 +23,48 @@ public class Signup extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button signup = (Button) findViewById(R.id.signup_signup_button);
+        final Button signup = (Button) findViewById(R.id.signup_signup_button);
+        final EditText etEmail = (EditText) findViewById(R.id.email_signUp);
+        final EditText etUsername = (EditText) findViewById(R.id.username_signUp);
+        final EditText etPassword = (EditText) findViewById(R.id.pass_signUp);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Action_SignupToHome(view);
 
-
                 final String username = etUsername.getText().toString();
-                final String email = email.getText().toString();
+                final String email = etEmail.getText().toString();
                 final String password = etPassword.getText().toString();
 
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if (success) {
-                                Intent intent = new Intent(Signup.this, Login.class);
-                                Signup.this.startActivity(intent);
-                            } else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(Signup.this);
-                                builder.setMessage("Register Failed")
-                                        .setNegativeButton("Retry", null)
-                                        .create()
-                                        .show();
+                    Response.Listener<String> responseListener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+                                if (success) {
+                                    Intent intent = new Intent(Signup.this, Login.class);
+                                    Signup.this.startActivity(intent);
+                                } else {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(Signup.this);
+                                    builder.setMessage("Register Failed")
+                                            .setNegativeButton("Retry", null)
+                                            .create()
+                                            .show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
-                        } catch (JSONException e) {
-                                    e.printStackTrace();
                         }
-                    }
-                };
-            }
+                    };
+
+                    RegisterRequest registerRequest = new RegisterRequest(username, email, password, responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(Signup.this);
+                    queue.add(registerRequest);
+                }
         });
     }
-
-
-
-    //@Override
-    public void Action_SignupToHome(View view)
-    {
-        Intent intent = new Intent(getApplicationContext(), Home.class);
-        startActivity(intent);
-    }
-
-
 }
+
+
