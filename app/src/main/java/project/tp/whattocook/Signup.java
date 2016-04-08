9a5c29551baca76e0409ba7034c585735b1a5cf2
kristiana <kriji98@abv.bg>
2016-04-08 13:58:10 +0300

    adding volley correctly

--- app/src/main/java/project/tp/whattocook/Signup.java
+++ app/src/main/java/project/tp/whattocook/Signup.java
@@ -1,13 +1,19 @@
 package project.tp.whattocook;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Signup extends AppCompatActivity {
 
@@ -19,13 +25,43 @@ public class Signup extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Button signup = (Button) findViewById(R.id.signup_signup_button);
+
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Action_SignupToHome(view);
+                //Action_SignupToHome(view);
+
+
+                final String username = etUsername.getText().toString();
+                final String email = email.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                Intent intent = new Intent(Signup.this, Login.class);
+                                Signup.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Signup.this);
+                                builder.setMessage("Register Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                                    e.printStackTrace();
+                        }
+                    }
+                };
             }
         });
     }
+
+
 
     //@Override
     public void Action_SignupToHome(View view)
@@ -33,4 +69,6 @@ public class Signup extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), Home.class);
         startActivity(intent);
     }
+
+
 }
