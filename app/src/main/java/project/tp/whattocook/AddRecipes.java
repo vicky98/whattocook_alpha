1abd9f03eb470b15539f29abfeee4d2844b138d4
vicky98 <vikiga98@gmail.com>
2016-05-10 22:14:23 +0300

    Some updates...

--- app/src/main/java/project/tp/whattocook/AddRecipes.java
+++ app/src/main/java/project/tp/whattocook/AddRecipes.java
@@ -1,13 +1,29 @@
 package project.tp.whattocook;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AddRecipes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_recipes);
+
+        Button addPhoto = (Button) findViewById(R.id.button_add_recipes_photo);
+
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                // finish the uploading of a picture
+            }
+
+        });
     }
+
 }
